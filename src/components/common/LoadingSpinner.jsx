6f4992448095df7a6d185bda50841960d181befc
Burhan-sheikh <epithet02@gmail.com>
2025-11-21 00:36:17 +0530

    Updated LoadingSpinner.jsx

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="flex items-center justify-center py-10">
+      <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary-500 border-t-transparent"></div>
+    </div>
+  );
+}
